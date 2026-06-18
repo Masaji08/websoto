0f7d434d4fac78631891d7def1019c42d1fd408c
Admin Soto <admin@soto.com>
2026-06-18 15:33:57 +0700

    Switch from Pusher to Reverb, add ngrok config, dynamic forceTLS

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -6,10 +6,13 @@ window.Pusher = Pusher;
 const token = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true,
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
+    forceTLS: import.meta.env.VITE_REVERB_SCHEME === 'https',
+    enabledTransports: ['ws', 'wss'],
     authEndpoint: '/broadcasting/auth',
     auth: {
         headers: {
