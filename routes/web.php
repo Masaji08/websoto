0552e25a3c980c7492066819ddde602647ef616b
Admin Soto <admin@soto.com>
2026-06-18 18:13:29 +0700

    redirect root to dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 // Publik - Menu Pelanggan
