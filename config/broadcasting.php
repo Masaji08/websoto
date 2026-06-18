c0e905d9d9c4472017eea66d3594a87e53092257
Admin Soto <admin@soto.com>
2026-06-18 16:30:18 +0700

    fix reverb default config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -6,14 +6,14 @@ return [
     'connections' => [
         'reverb' => [
             'driver' => 'reverb',
-            'key' => env('REVERB_APP_KEY'),
-            'secret' => env('REVERB_APP_SECRET'),
-            'app_id' => env('REVERB_APP_ID'),
+            'key' => env('REVERB_APP_KEY', 'websoto-key'),
+            'secret' => env('REVERB_APP_SECRET', 'websoto-secret'),
+            'app_id' => env('REVERB_APP_ID', 'websoto'),
             'options' => [
-                'host' => env('REVERB_HOST', 'localhost'),
+                'host' => env('REVERB_HOST', '0.0.0.0'),
                 'port' => env('REVERB_PORT', 8080),
-                'scheme' => env('REVERB_SCHEME', 'http'),
-                'useTLS' => env('REVERB_SCHEME', 'http') === 'https',
+                'scheme' => env('REVERB_SCHEME', 'https'),
+                'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
                 'timeout' => 10,
